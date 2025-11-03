1e7614ed310b924f05756153dc2c8aadcb38b5a4
Michael Feeney <feeneym225@gmail.com>
2025-11-02 20:06:48 -0500

    start writing a type for a table, and start a function for printing a row

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # 2025-10-18:
-- Not sure what this will turn into, right now just making a crude database system using structs and maybe some custom binary file handling. -MF 
+
+* Not sure what this will turn into, right now just making a crude database system using structs and maybe some custom binary file handling. -MF
+
+
+
+
+
+
+
+2025-11-02:
+
+* for now using campaign finance data as test data: https://www.fec.gov/campaign-finance-data/all-candidates-file-description/
--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@ typedef char STATE[2];
 // TODO: see if there is a more flexible way to adjust the 
 // number of bytes
 typedef char CHAR32[32];
+typedef char PARTY[3];
 
 // Make a prototype record for now
 struct test_table
@@ -31,6 +32,25 @@ struct test_table
     BIGINT count;
     REAL avg_score;
 };
+
+struct candidate_master
+{
+    INDEX index; // NOTE: maybe this index will match with the index of the array that is the table itself
+    CHAR32 name;
+    PARTY party_code; // for DEM, REP, or IND
+    REAL total_receipts;
+    STATE state;
+
+};
+
+// TODO: start with this function, which prints a single row of a struct
+void print_row(candidate_master row_index)// NEED TO CHECK IF THESE PARAMETERS MAKE SENSE
+{
+    // TODO: so far this takes only a certain struct type
+    // so the subypes are already known, 
+    // for a more general function I might need to
+    // pass the names of the struct? 
+}
 
 int main(void)
 {
@@ -48,6 +68,28 @@ int main(void)
 
     printf("test_record.index: %I64d\n", test_record.index);
     printf("test_record.name: %s\n", test_record.name);
+
+
+    // test row to insert into the test table
+    candidate_master PetolaMary = {};
+
+    // for now I'm thinking first row will be index 1, and index zero can be metatdata
+    PetolaMary.index = 1;
+    strncpy(PetolaMary.name, "Petola, Mary", sizeof(PetolaMary.name) - 1);
+    strncpy(PetolaMary.party_code, "DEM", sizeof(PetolaMary.party_code) - 1);
+    PetolaMary.total_receipts = 136626.13;
+    strncpy(PetolaMary.state, "AK", sizeof(PetolaMary.state) - 1);
+
+    // create a table, which will be an array of pointers to structs
+    // that are the rows
+    int candidate_master_rows = 5;
+
+    candidate_master *CandidateMaster[5] = {};
+
+    CandidateMaster[1] = &PetolaMary;
+
+    // TODO: WRITE THIS FUNCTION, which takes a table name and prints a single row
+    print_row(CandidateMaster);
 
     return 0;
 }
